1c0b7428b88a4046dadfde496a25244a1e2a63f8
rshivraman <rajendran@hotmail.com>
2016-05-15 20:51:09 -0400

    Add files via upload
    Plot Histogram using the data file created in load_data

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 
 png(filename='plot1.png', width=480, height=480, units='px')
 
-## Now, plot the Histogram
+## Now, plot the Histogram using the data file loaded in load_data.R
 
 hist(as.numeric(as.character(datasubset$Global_active_power)), main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
 
